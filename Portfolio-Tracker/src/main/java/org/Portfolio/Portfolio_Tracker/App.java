45ce5ca91971073f23d3b96caf5ad3d75d0b1792
rohit74899 <rohitlande02@gmail.com>
2025-09-13 17:07:17 +0530

    Project setup

---
+++ Portfolio-Tracker/src/main/java/org/Portfolio/Portfolio_Tracker/App.java
@@ -0,0 +1,13 @@
+package org.Portfolio.Portfolio_Tracker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
