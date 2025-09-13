1c06b877b0f266ad225b0f2bd69fa1a882388dd2
rohit74899 <rohitlande02@gmail.com>
2025-09-13 20:11:09 +0530

    feat : Database Setup directly Done on main (Mistakly)

---
+++ DataBase Setup/Db Scripts/DDL_Table_Entity_Master.sql
@@ -0,0 +1,14 @@
+
+create table Entity_Master
+( user_id Varchar(100) ,
+  username varchar(100),
+  passwd varchar(100),
+  email varchar(100)
+);
+
+DESC Entity_Master;
+ALTER TABLE Entity_Master
+MODIFY user_id NUMBER NOT NULL;
+
+Alter table Entity_Master
+Add CONSTRAINT Pk_Entity PRIMARY KEY (user_id,username);
