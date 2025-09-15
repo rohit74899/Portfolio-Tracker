c8bd5b339d363e30cc4076ab46a4172425023f26
rohit74899 <rohitlande02@gmail.com>
2025-09-16 00:23:50 +0530

    feat : GET Method Implented (based On Id data will be returned)

--- Portfolio-Tracker/src/main/java/org/Portfolio/dao/EntityMasterDao.java
+++ Portfolio-Tracker/src/main/java/org/Portfolio/dao/EntityMasterDao.java
@@ -1,8 +1,27 @@
 package org.Portfolio.dao;
 
+import java.util.Scanner;
+
+import org.Portfolio.entity.Entity_Master;
+import org.Portfolio.util.HibernateUtil;
+import org.hibernate.Session;
+
 public class EntityMasterDao {
 	
-	public void getEntityData() {
+	public Entity_Master getEntityData() {
+		
+		Session session= HibernateUtil.getSessionFactory().openSession();
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the Id of the User !");
+		int id=sc.nextInt();
+		sc.nextLine();
+		
+		Entity_Master e=session.get(Entity_Master.class, id);
+		
+		return e;
+		
 		
 	}
 	
