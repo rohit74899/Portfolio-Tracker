586a84ca84744874c347f7ca24fae5a8fb725005
rohit74899 <rohitlande02@gmail.com>
2025-09-23 22:39:16 +0530

    feat: Menu Driver Program added to Test/Use Functionality

--- Portfolio-Tracker/src/main/java/org/Portfolio/Portfolio_Tracker/App.java
+++ Portfolio-Tracker/src/main/java/org/Portfolio/Portfolio_Tracker/App.java
@@ -1,5 +1,7 @@
 package org.Portfolio.Portfolio_Tracker;
 
+import java.util.Scanner;
+
 import org.Portfolio.dao.EntityMasterDao;
 import org.Portfolio.dao.PortfolioTxnDao;
 import org.Portfolio.util.HibernateUtil;
@@ -8,21 +10,59 @@ public class App
 {
     public static void main( String[] args )
     {
+        
+        Scanner sc=new Scanner(System.in);
         System.out.println( "== Welcome to Portfolio Tracker ==" );
         
         EntityMasterDao dd= new EntityMasterDao();
-        //dd.insertEntity();
-        
-        //dd.deleteEntity();
-        //dd.updateEntity();
-        
-        
         PortfolioTxnDao tt=new PortfolioTxnDao();
+
         
-        //tt.insert_PortTxn();
+        boolean flg=true;
+        while(flg) {
+        	
+        	System.out.println("\n\tChoose Operation To Perform"
+        			+ "\n\t------|     User Operations      |--------------\n"
+        			+ "\n\t 1.Register a new User "
+        			+ "\n\t 2.Get User Details"
+        			+ "\n\t 3.Update User Details"
+        			+ "\n\t 4.Delete User Details"
+        			+ "\n\t------|   Portfolio Operations   |-------------\n"
+        			+ "\n\t 5.Insert Portfolio Transaction"
+        			+ "\n\t 6.Get Transaction Details By ID"
+        			+ "\n\t 7.Update Transaction Details By ID"
+        			+ "\n\t 8.Delete Transaction Details By ID"
+        			+ "\n\t 9.Exit App");
+        	System.out.println("Enter Option:");
+        	int op=sc.nextInt(); sc.nextLine();
+        	
+        	switch(op) {
+        	case 1:dd.insertEntity();
+        		break;
+        	case 2:dd.getEntityData();
+        		break;
+        	case 3:dd.updateEntity();
+        		break;
+        	case 4:dd.deleteEntity();
+        		break;
+        	case 5:tt.insert_PortTxn();
+        		break;
+        	case 6:tt.get_PortTxnById();
+        		break;
+        	case 7:tt.update_PortTxn();
+        		break;
+        	case 8:tt.delete_PortTxnById();
+        		break;
+        	case 9:flg=false;
+        		break;
+        	default: System.out.println("Running ....");
+        		break;
+        	
+        		
+        	}
+        }
         
-        //tt.delete_PortTxnById();
-        tt.update_PortTxn();
+        
         
     }
 }
