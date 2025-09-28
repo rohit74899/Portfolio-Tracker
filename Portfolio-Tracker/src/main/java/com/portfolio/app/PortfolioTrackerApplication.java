8893de13958d82b255656ba9bcdae0562434de4a
rohit74899 <rohitlande02@gmail.com>
2025-09-28 10:52:39 +0530

    feat : Spring boot migration first Commit

---
+++ Portfolio-Tracker/src/main/java/com/portfolio/app/PortfolioTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.portfolio.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioTrackerApplication.class, args);
+		System.out.println("Hello Rohit");
+	}
+
+}
---
+++ Portfolio-Tracker/src/test/java/com/portfolio/app/PortfolioTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
